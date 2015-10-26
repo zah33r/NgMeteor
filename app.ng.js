a1f9b35454aea8b423b0dfe4b075b8f2073b3e17
zah33r <zaheeer1@gmail.com>
2015-10-26 21:31:03 +0500

    updated controller to load collection from MongoDB

--- app.ng.js
+++ app.ng.js
@@ -1,20 +1,9 @@
+Parties = new Mongo.Collection("parties");  //on both client and server side (Mongo and moniMongo)
+
 if (Meteor.isClient) {
   angular.module('socially', ['angular-meteor']);
 
-  angular.module('socially').controller('PartiesListCtrl', ['$scope', function ($scope) {
-    $scope.parties = [
-      {
-        'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.'
-      },
-      {
-        'name': 'All dubstep all the time',
-        'description': 'Get it on!'
-      },
-      {
-        'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.'
-      }
-    ];
-  }]);
+  angular.module('socially').controller('PartiesListCtrl',function ($scope, $meteor) {
+    $scope.parties = $meteor.collection(Parties);
+  });
 }
